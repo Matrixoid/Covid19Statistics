c76e85d0089f9d78311b046efc31db093f22b113
Matrixoid <mr.art.pro@mail.ru>
2023-11-12 10:25:01 +0300

    Создана структура проекта

---
+++ Covid19Statistics/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Covid19Statistics.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Statistics.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+    }
+}
